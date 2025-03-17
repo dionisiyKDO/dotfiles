8ee73a790170291e9f9f1bda7f7d10c4821d1aaa
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-03-17 12:40:00 +0200

    Update waybar configuration and styling

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -1,21 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.waybar = {
-    enable = true;
-    
-    home.file = {
-      ".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
-      ".config/waybar/style.css".source = ./waybar/style.css;
-      
-      ".config/waybar/scripts/random-wallpaper.sh" = {
-        source = ./waybar/scripts/random-wallpaper.sh;
-        executable = true;  # ensures the file has executable permissions
-      };
-      ".config/waybar/scripts/favorite-wallpaper.sh" = {
-        source = ./waybar/scripts/favorite-wallpaper.sh;
-        executable = true;
-      };
+  programs.waybar.enable = true;
+  
+  home.file = {
+    ".config/waybar/config.jsonc" = {
+      source = ./waybar/config.jsonc;
     };
+    ".config/waybar/style.css" = {
+      source = ./waybar/style.css;
+    };
+
+    # TODO: Doesn't work. Try to find why
+    # It doesn't create link in ~/.config/waybar/scripts
+    # ".config/waybar/scripts/random-wallpaper.sh" = {
+    #   source = ./waybar/scripts/random-wallpaper.sh;
+    #   executable = true;
+    # };
+    # ".config/waybar/scripts/favorite-wallpaper.sh" = {
+    #   source = ./waybar/scripts/favorite-wallpaper.sh;
+    #   executable = true;
+    # };
   };
 }
